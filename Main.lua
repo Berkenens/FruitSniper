3d41fc1f53d3a819309e6b584413a6b0ad7f653a
Berkenens <78962912+Berkenens@users.noreply.github.com>
2026-05-16 18:47:12 +0300

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,15 +1,5 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local CommF = ReplicatedStorage:WaitForChild("Remotes"):WaitForChild("CommF_")
 
-local CommF = ReplicatedStorage
-    :WaitForChild("Remotes")
-    :WaitForChild("CommF_")
-
-local success, err = pcall(function()
-    CommF:InvokeServer("SetTeam", "Pirates")
-end)
-
-if success then
-    print("[SNIPER] Team: Pirates")
-else
-    warn("[SNIPER] Takim err:", err)
-end
+CommF:InvokeServer("SetTeam", "Pirates")
+print("Takim secildi: Pirates")
